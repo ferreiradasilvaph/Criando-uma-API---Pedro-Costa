34375d22f188da24a80b7e550a6937e6e5ba3785
Pedro Costa <137563186+ferreiradasilvaph@users.noreply.github.com>
2023-12-07 23:59:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// utilizando biblioteca exoress com modulo require
 const express = require('express');
+// criando um servidor que recebe a biblioteca
 const server = express();
+// hospedando objeto json
 const filmes = require('./src/data/filmes.json')
 
+// criando requisição get dentro do servidor com uma função anônima que recebe parâmetros de requisição e resposta.
 server.get('/filmes', (req , res) =>{
+    // retorna a resposta em formato json atribuindo o objeto hospedado na const filmes
     return res.json(filmes)
 })
 // porta que o servidor está escutando
 server.listen(3000, () => {
+    // comando console.log para testar a funcionalidade da API.
 console.log("servidor esta funcionando...");
 })
